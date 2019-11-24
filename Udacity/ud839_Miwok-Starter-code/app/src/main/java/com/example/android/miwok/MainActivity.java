19c175c3753e12f9d487fba2bbe1980e2f07cfb7
Harrison <harrisontran@outlook.com>
2019-11-23 23:50:04 -0800

    Miwok app arrayadapter done

--- Udacity/ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Udacity/ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,14 +18,20 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
